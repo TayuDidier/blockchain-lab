86679b60a088804f1f7214d68428f7e4133f645a
TayuDidier <tayudidier3@gmail.com>
2026-06-03 03:11:26 +0100

    all tasks completed

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,10 +1,38 @@
+// const { ethers } = require("hardhat");
+
+// async function main() {
+//   // Deploy SimpleStorage
+//   const Storage = await ethers.getContractFactory("SimpleStorage");
+//   const simpleStorage = await Storage.deploy();
+//   await simpleStorage.waitForDeployment();
+//   console.log("SimpleStorage deployed to:", await simpleStorage.getAddress());
+
+//   // Deploy SecureStorage
+//   const SecureStorage = await ethers.getContractFactory("SecureStorage");
+//   const secureStorage = await SecureStorage.deploy();
+//   await secureStorage.waitForDeployment();
+//   console.log("SecureStorage deployed to:", await secureStorage.getAddress());
+// }
+
+// main().catch((err) => {
+//   console.error(err);
+//   process.exit(1);
+// });
 const { ethers } = require("hardhat");
 
 async function main() {
-  const Storage = await ethers.getContractFactory("SimpleStorage");
-  const contract = await Storage.deploy();
-  await contract.waitForDeployment();
-  console.log("Contract deployed to:", await contract.getAddress());
+  const LabToken = await ethers.getContractFactory("LabToken");
+  const labToken = await LabToken.deploy();
+  await labToken.waitForDeployment();
+
+  const tokenAddress = await labToken.getAddress();
+  console.log("LabToken deployed to:", tokenAddress);
+
+  const [deployer] = await ethers.getSigners();
+  console.log("Deployer address:", deployer.address);
+
+  const balance = await labToken.balanceOf(deployer.address);
+  console.log("Deployer balance:", ethers.formatEther(balance), "LTK");
 }
 
 main().catch((err) => {
